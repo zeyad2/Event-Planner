739fa5b7cd1e171bb18ed03a9bc2201bb18bf0ac
zeyad2 <zeyadsattar.me@gmail.com>
2025-10-29 21:32:14 +0300

    jwt authentication

--- server/app/models/userModel.py
+++ server/app/models/userModel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, ForeignKey, func
 from app.db.session import Base
 
 class User(Base):
@@ -7,3 +7,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
